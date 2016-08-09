9b12a0d03c323d00d43d1d30256673ccfc68c01e
Matt Schwartz <ohmattschwartz@gmail.com>
2016-08-09 10:18:36 -0400

    blogging

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Intro to Matt</h1>
+        </div>
+        <div className="texthead">
+          <h2>August 8th, 2016</h2>
+        </div>
+        <p className="App-intro">
+          It's difficult to reflect on React (to react about react, if you will- will you?) without truly comprehending what it is.  I spent the past half hour reading about it on Wikipedia and learned a great deal about what it's used for (Netflix, AirBnB), how often it's used (it's apparently the sixth most starred item on GitHub- as in, of all time) but I'm going to require a few more days to comprehend exactly what makes it special.  It was mentioned in class today that we may find an urge to go back and re-work some old homework assignments once we have React under our belts.  As of press time- can you say that when it comes to pretend blogs?- I see no reason why it's any easier than using HTML, CSS, and JavaScript in three separate, easy to read files.  Something tells me that's Day-One talking, and my true opinion of the thing has yet to begin to take shape.  I mentioned to some classmates the other day that I find myself on a two to three day comprehension lag when it comes to The Iron Yard program in general.  With JavaScript I'm almost on a three to four day lag.  The good news there,though, is that I do eventually comprehend the material!  It just takes a minute.  I hope to continue this notion with the world of React.
+        </p>
+        <p className="App-paragraph">
+          More than anything else, I'm really excited about finding out some practical applications for this new feature of JavaScript (including use in a blog).  When we apply real-world scenarios where using the technology we're learning is helpful, I'm able to better grasp the core concepts of the technology.  I haven't had that "A Ha" moment that everyone keeps talking about - though apparently I'm not supposed to yet- but I have a feeling that because of what I just stated, it may not come until I'm building my final app and am therefore ready to make use of the different pieces of tech - including React - that we've learned.  With all that in mind, regarding the article we read today's lecture...well I've still got three days of comprehension lag to deal with before I'm able to fully grasp what the heck I'm doing, even as I type.
+      </p>
+      <div className="footer">
+        <h3>Matt Schwartz Iron Yard Blog</h3>
+        <p>
+        <h4>8/8/16</h4>
+        </p>
+      </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
